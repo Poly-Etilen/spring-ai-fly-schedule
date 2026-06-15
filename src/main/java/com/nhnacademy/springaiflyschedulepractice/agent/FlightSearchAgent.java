27754698df84a71117689a05a5dacbd5331556a1
Poly-Etilen <wlsdud04060@naver.com>
2026-06-15 10:40:16 +0900

    fix: 메서드 및 controller 수정

--- src/main/java/com/nhnacademy/springaiflyschedulepractice/agent/FlightSearchAgent.java
+++ src/main/java/com/nhnacademy/springaiflyschedulepractice/agent/FlightSearchAgent.java
@@ -1,17 +1,12 @@
 package com.nhnacademy.springaiflyschedulepractice.agent;
 
-import com.nhnacademy.springaiflyschedulepractice.dto.FlightDetail;
 import com.nhnacademy.springaiflyschedulepractice.dto.FlightInfoResponse;
-import com.nhnacademy.springaiflyschedulepractice.dto.FlightSearchParam;
-import com.nhnacademy.springaiflyschedulepractice.dto.airline.AirlineGroup;
 import com.nhnacademy.springaiflyschedulepractice.service.ApiClientService;
-import com.nhnacademy.springaiflyschedulepractice.service.ParameterNormalizerAgent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Service
--- src/main/java/com/nhnacademy/springaiflyschedulepractice/controller/NlSearchController.java
+++ src/main/java/com/nhnacademy/springaiflyschedulepractice/controller/NlSearchController.java
@@ -1,9 +1,9 @@
 package com.nhnacademy.springaiflyschedulepractice.controller;
 
 
-import com.nhnacademy.springaiflyschedulepractice.dto.OrchestrationResult;
-import com.nhnacademy.springaiflyschedulepractice.service.NLOrchestrationService;
+import com.nhnacademy.springaiflyschedulepractice.dto.AiFlightSearchResult;
 import com.nhnacademy.springaiflyschedulepractice.service.CoordinatorSearchService;
+import com.nhnacademy.springaiflyschedulepractice.service.NLOrchestrationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,13 +20,13 @@ public class NlSearchController {
     private final CoordinatorSearchService coordinatorSearchService;
 
     @PostMapping("/search")
-    public OrchestrationResult search(@RequestBody Map<String, String> request) {
+    public AiFlightSearchResult search(@RequestBody Map<String, String> request) {
         String message = request.get("message");
         String model = request.getOrDefault("model", "gemini");
         String type = request.getOrDefault("type", "orchestrator");
 
         if (message == null || message.isBlank()) {
-            return OrchestrationResult.error("메세지를 입력해주세요");
+            return AiFlightSearchResult.error("메세지를 입력해주세요");
         }
         if (type.equals("coordinator")) {
             return coordinatorSearchService.executeCoordinatorSearch(message, model);
